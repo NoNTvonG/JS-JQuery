4961b359243f8196c47219330359580e2d182bdb
NoNTvonG <nontvong@gmail.com>
2021-11-09 20:05:53 +0100

    JQuery - list v2

--- list/js/list.js
+++ list/js/list.js
@@ -1,32 +1,21 @@
 const customRow = (id) => {
-  return `<div class="input-group mb-3 customRow">
+  return `<div class="input-group mb-3 customRow" counter="${id}">
           <span for="" class="input-group-text">${id}</span>
           <input type="text" class="form-control px-3" />
           </div>`
 }
 
-const createArrLength = (num) => {
-  let arr = []
-  for (let i = 0; i < num; i++) {
-    arr.push(i + 1)
-  }
-  return arr
-}
-
 const addNewRow = () => {
-  let x = parseInt($("#peopleCounter").val()) // взнаємо вказану цифру
-  let x1 = createArrLength(x)
-  let prevX = $("#peopleArr .customRow").length // попередня кількість елементів
+  let x = $("#peopleCounter").val() // взнаємо вказану цифру
+  let prev_x = $("#peopleArr .customRow").length // попередня кількість елементів
 
-  $("#peopleArr .customRow").remove() // очищаємо весь список
-
-  x1.forEach((element) => {
-    $("#peopleArr").append(customRow(element))
-  })
-
-  // if (x > prevX) {
-  //   $("#peopleArr").append(customRow(x))
-  // } else if (x < prevX) {
-  //   $("#peopleArr .customRow").last().remove()
-  // }
+  if (x > prev_x) {
+    for (let i = prev_x + 1; i <= x; i++) {
+      $("#peopleArr").append(customRow(i))
+    }
+  } else if (x < prev_x) {
+    for (let i = prev_x; i > x; i--) {
+      $(`div[counter="${i}"]`).remove()
+    }
+  }
 }
